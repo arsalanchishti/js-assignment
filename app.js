39bf13c16c0acf962cd704930fb4e00f69921839
arsalanchishti <shedowmasterno1@gmail.com>
2020-06-14 19:56:46 +0500

    not complet

---
+++ app.js
@@ -0,0 +1,326 @@
+// ===== chepter no 1 =====
+
+// ====task no 1====
+
+
+
+// alert("welcome to js assignment")
+
+
+// ====task no 2====
+
+
+// alert("Error! please enter a valid password")
+
+
+// ====task no 3====
+
+// alert("welcome to js land \n Happy Coding!")
+
+
+
+
+// ====task no 4====
+
+// alert("welcome to js land")
+
+// alert("Happy Coding!")
+
+
+
+
+
+
+
+
+
+//==== chepter no 2 ====
+
+
+// ====task no 1====
+
+// var username = "arsalan";
+
+
+
+
+// ====task no 2====
+
+// var myname = "muhammad arsalan ansari";
+
+
+// ====task no 4====
+
+// var message = "hello world";
+// alert(message);
+
+
+// ====task no 4====
+
+// var name = "jhone doe";
+// var age = " 15 years old";
+// var job = "certified mobile application development"
+
+// alert(name);
+// alert(age);
+// alert(job);
+
+
+// ====task no 5====
+
+// var a = "PIZZA \nPIZZ\nPIZ\nPI\nP";
+// alert(a);
+
+
+// ====task no 6====
+
+// var email = "My email adderss is example@example.com";
+// alert(email);
+
+
+// ====task no 7====
+
+// var a = "i'm trynig to learn from the book a smarter \n way to learn javascript";
+// alert(a);
+
+
+// ====task no 8====
+
+// document.write('Yeh ! i can write HTML content throught javascript');
+
+
+// ====task no 9====
+
+// this si skiped
+
+
+
+
+
+
+
+
+//==== chepter no 3====
+
+
+// ====task no 1====
+
+// var age = "i am 15 years old";
+// alert(age);
+
+
+
+// ====task no 2====
+
+// alert("you have visited this site 14 times");
+
+
+
+// ====task no 3====
+
+// var birthyear = 1993;
+
+// document.write("my birth year is " + birthyear);
+
+
+
+
+// ====task no 4====
+
+// var name = "jhon";
+// var title = "T-shert";
+// var quantity = 5;
+
+// document.write(name + " " + "ordered" + " " + quantity + " " + title + " " + "in the XYZ store online");
+
+
+
+
+
+
+
+
+
+//==== chepter no 4 =====
+
+
+
+// ====task no 1 ====
+
+// var a = "the",
+    // b = "hello",
+    // c = "world";
+
+
+
+// ==== task no 2 ====
+
+// document.write('<h1> tha legal variable </h1>')
+// document.write('$a <br />')
+// document.write('_name <br />')
+// document.write('A <br />')
+// document.write('username123 <br />')
+// document.write('camAlcase <br />')
+
+
+// document.write('<h1> the illigl variable </h1>')
+// document.write('123name <br />')
+// document.write('user name <br />')
+// document.write('@name <br />')
+// document.write('"my name"')
+// document.write('%a <br />')
+
+
+
+// ==== task no 3 ====
+
+// document.write('<h1>Rules for naming JS variables</h1>')
+// document.write('<br />')
+// document.write('Variable names can only contain , number , $ and _ For example $my_1stVariable')
+// document.write('<br />')
+// document.write('Variables must begin with a  latter $ and _ For example $name, _name or 1name')
+// document.write('<br />')
+// document.write('Variable names are case  sensitive')
+// document.write('<br />')
+// document.write('Variable names should not be JS keywords')
+
+
+
+
+
+
+
+
+
+
+
+//==== chepter no 5 ====
+
+
+
+// ==== task no 1 ====
+
+// var a = 3;
+// var b = 5;
+// var c = a+b;
+
+// document.write('sum of' + " " + a + " " + "and" + " " + b + " " + "is" + " " + c)
+
+
+
+// ==== task no 2 ====
+
+// var a = 3;
+// var b = 5;
+// var c = a+b;
+
+// document.write('sum of' + " " + a + " " + "and" + " " + b + " " + "is" + " " + c)
+
+
+// var a = 3;
+// var b = 5;
+// var c = a*b;
+
+// document.write('multiplication of' + " " + a + " " + "and" + " " + b + " " + "is" + " " + c)
+
+
+
+// var a = 3;
+// var b = 5;
+// var c = a/b;
+
+// document.write('division of' + " " + a + " " + "and" + " " + b + " " + "is" + " " + c)
+
+
+
+
+
+// ===== task no 3 =====
+
+
+// var a = 0;
+
+// document.write('Value after variable declaration is' + " " + a + '<br />')
+
+// var a = 5;
+
+// document.write('Initial value' + " " + a + '<br />')
+
+// var a = 5+1;
+
+// document.write('Value after increment is' + " " + a + '<br />')
+
+
+// var a = 5+1+7;
+
+// document.write('Value after addition is' + " " + a + '<br />')
+
+
+
+// var a = 5+1+7-1;
+
+// document.write('Value after decrement is' + " " + a + '<br />')
+
+// var a = 12/3;
+
+// document.write('The remainder is ' + " " + a + '<br />')
+
+
+
+
+// ===== task no 4 =====
+
+// var ticket = 600;
+// var total = ticket*5;
+
+// document.write('Total cost to buy 5 tickets to a movie is ' + " " + total + 'PKR')
+
+
+
+// ===== task no 5 =====
+
+// var a = 2;
+
+// document.write('table of 2 <br />')
+// document.write('2 x 1 = ' + " " + a*1 + '<br />')
+// document.write('2 x 2 = ' + " " + a*2 + '<br />')
+// document.write('2 x 3 = ' + " " + a*3 + '<br />')
+// document.write('2 x 4 = ' + " " + a*4 + '<br />')
+// document.write('2 x 5 = ' + " " + a*5 + '<br />')
+// document.write('2 x 6 = ' + " " + a*6 + '<br />')
+// document.write('2 x 7 = ' + " " + a*7 + '<br />')
+// document.write('2 x 8 = ' + " " + a*8 + '<br />')
+// document.write('2 x 9 = ' + " " + a*9 + '<br />')
+// document.write('2 x 10 = ' + " " + a*10 + '<br />')
+
+
+
+// ===== task no 7 ====
+
+// var item1 = 650;
+// var qun1 = 3
+// var item2 = 100;
+// var qun2 = 7;
+// var shiping = 100;
+// var total = (item1 * qun1) + (item2 * qun2) + shiping;
+
+
+
+// document.write('<h1>shoping cart</h1>')
+// document.write('<br />')
+// document.write('<br />')
+// document.write('price of item 1 is' + item1 + '<br />')
+// document.write('quantity of item 1 is' + qun1 + '<br />')
+// document.write('price of item 2 is' + item2 + '<br />')
+// document.write('quantity of item 2 is' + qun2 + '<br />')
+// document.write('shiping charges is' + shiping + '<br />')
+// document.write('<br />')
+// document.write('<br />')
+// document.write('<br />')
+// document.write('total cost of you order' + total +'<br />')
+
+
+
+// ==== task no 8 =====
+
+document.write('sir eid k bad se me bimar para ho mujhe namonya ho gaya tha us k bad abhi tak meri tabyat thek nhi ho pai he jism me bohat kamzori ho gai he zyada der betha nhi jata mene javascript ki sari class downlod karli thi lekin un ki practic nhi kar paya is ye assaignment jitna ho sakta mene kardya he agar himat hoti to baki ki tarha ye assignment bhi kardeta')
